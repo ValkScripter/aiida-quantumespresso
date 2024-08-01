86630be3eb61839d1837c23b04333ef6e9d23305
Ian-Evan MICHEL <ian-evan.michel@cea.fr>
2024-08-01 14:30:01 +0200

    Corrected AverageCalculation remote_copy_list

--- src/aiida_quantumespresso/calculations/average.py
+++ src/aiida_quantumespresso/calculations/average.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+"""`CalcJob` implementation for the average.x code of Quantum ESPRESSO."""
+import os
+
 from aiida import orm
 from aiida.common import datastructures
 from aiida.engine import CalcJob
-from aiida.orm import SinglefileData
-from ase.units import Ry, Bohr
+from ase.units import Bohr
 
 
 class AverageCalculation(CalcJob):
@@ -11,10 +14,10 @@ class AverageCalculation(CalcJob):
     # pylint: disable=protected-access
     from aiida_quantumespresso.calculations import BasePwCpInputGenerator
 
-    _DEFAULT_INPUT_FILE = "avg.in"
-    _DEFAULT_OUTPUT_FILE = "avg.out"
-    _DEFAULT_INPUT_DATA_FILE = "aiida.filplot"
-    _DEFAULT_OUTPUT_DATA_FILE = "avg.dat"
+    _DEFAULT_INPUT_FILE = 'avg.in'
+    _DEFAULT_OUTPUT_FILE = 'avg.out'
+    _DEFAULT_INPUT_DATA_FILE = 'aiida.filplot'
+    _DEFAULT_OUTPUT_DATA_FILE = 'avg.dat'
     # pylint: enable=protected-access
 
     @classmethod
@@ -24,93 +27,107 @@ class AverageCalculation(CalcJob):
 
         # Input ports
         spec.input(
-            "parent_folder",
+            'parent_folder',
             valid_type=(orm.RemoteData, orm.FolderData),
             required=True,
             help="Output folder of a completed `PpCalculation` where the 'aiida.filplot' file will be copied from.",
         )
         spec.input(
-            "average_axis",
+            'average_axis',
             valid_type=orm.Int,
             required=False,
             default=lambda: orm.Int(3),
-            help="Planar average done in plane orthogonal to this axis (1,2 or 3)",
+            help='Planar average done in plane orthogonal to this axis (1,2 or 3)',
         )
         spec.input(
-            "npts",
+            'npts',
             valid_type=orm.Int,
             required=False,
             default=lambda: orm.Int(1000),
-            help="Number of interpolation points of the planar and macroscopic averages.\
-                If less points than the nb of FFT points in average_axis direction, no interpolation is done.",
+            help='Number of interpolation points of the planar and macroscopic averages.\
+                If less points than the nb of FFT points in average_axis direction, no interpolation is done.',
         )
         spec.input(
-            "window_size",
+            'window_size',
             valid_type=orm.Float,
             required=True,
-            help="Window size for the macroscopic average in Angstrom (the code reads in Bohr, conversion is done internally)",
+            help='Window size for the macroscopic average in Angstrom \
+            (the code reads in Bohr, conversion is done internally)',
         )
 
         spec.input(
-            "metadata.options.input_filename",
+            'metadata.options.input_filename',
             valid_type=str,
             default=cls._DEFAULT_INPUT_FILE,
         )
         spec.input(
-            "metadata.options.output_filename",
+            'metadata.options.output_filename',
             valid_type=str,
             default=cls._DEFAULT_OUTPUT_FILE,
         )
         spec.input(
-            "metadata.options.parser_name",
+            'metadata.options.parser_name',
             valid_type=str,
-            default="quantumespresso.average",
+            default='quantumespresso.average',
         )
-        spec.input("metadata.options.withmpi", valid_type=bool, default=False)
+        spec.input('metadata.options.withmpi', valid_type=bool, default=False)
 
         # Output ports
         spec.output(
-            "output_parameters",
+            'output_parameters',
             valid_type=orm.Dict,
         )
         spec.output(
-            "output_data",
+            'output_data',
             valid_type=orm.ArrayData,
-            help="The output data containing columns: z Ang, p(z) eV, m(z) eV. code outputs are in Bohr and Rydberg. Conversion is done internally.",
+            help='The output data containing columns: z Ang, p(z) eV, m(z) eV. \
+            Code outputs are in Bohr and Rydberg. Conversion is done internally.',
         )
 
         # Standard exceptions
-        spec.exit_code(301, 'ERROR_NO_RETRIEVED_TEMPORARY_FOLDER',
-            message='The retrieved temporary folder could not be accessed.')
-        spec.exit_code(302, 'ERROR_OUTPUT_STDOUT_MISSING',
-            message='The retrieved folder did not contain the required stdout output file.')
-        spec.exit_code(303, 'ERROR_OUTPUT_XML_MISSING',
-            message='The parent folder did not contain the required XML output file.')
-        spec.exit_code(310, 'ERROR_OUTPUT_STDOUT_READ',
-            message='The stdout output file could not be read.')
-        spec.exit_code(311, 'ERROR_OUTPUT_STDOUT_PARSE',
-            message='The stdout output file could not be parsed.')
-        spec.exit_code(312, 'ERROR_OUTPUT_STDOUT_INCOMPLETE',
-            message='The stdout output file was incomplete.')
-        spec.exit_code(340, 'ERROR_OUT_OF_WALLTIME_INTERRUPTED',
+        spec.exit_code(
+            301, 'ERROR_NO_RETRIEVED_TEMPORARY_FOLDER', message='The retrieved temporary folder could not be accessed.'
+        )
+        spec.exit_code(
+            302,
+            'ERROR_OUTPUT_STDOUT_MISSING',
+            message='The retrieved folder did not contain the required stdout output file.'
+        )
+        spec.exit_code(
+            303, 'ERROR_OUTPUT_XML_MISSING', message='The parent folder did not contain the required XML output file.'
+        )
+        spec.exit_code(310, 'ERROR_OUTPUT_STDOUT_READ', message='The stdout output file could not be read.')
+        spec.exit_code(311, 'ERROR_OUTPUT_STDOUT_PARSE', message='The stdout output file could not be parsed.')
+        spec.exit_code(312, 'ERROR_OUTPUT_STDOUT_INCOMPLETE', message='The stdout output file was incomplete.')
+        spec.exit_code(
+            340,
+            'ERROR_OUT_OF_WALLTIME_INTERRUPTED',
             message='The calculation stopped prematurely because it ran out of walltime but the job was killed by the '
-                    'scheduler before the files were safely written to disk for a potential restart.')
-        spec.exit_code(350, 'ERROR_UNEXPECTED_PARSER_EXCEPTION',
-            message='The parser raised an unexpected exception: {exception}')
+            'scheduler before the files were safely written to disk for a potential restart.'
+        )
+        spec.exit_code(
+            350, 'ERROR_UNEXPECTED_PARSER_EXCEPTION', message='The parser raised an unexpected exception: {exception}'
+        )
 
         # Output datafile related exceptions
-        spec.exit_code(330, 'ERROR_OUTPUT_DATAFILE_MISSING',
-            message='The formatted data output file `{filename}` was not present in the retrieved (temporary) folder.')
-        spec.exit_code(331, 'ERROR_OUTPUT_DATAFILE_READ',
-            message='The formatted data output file `{filename}` could not be read.')
-        spec.exit_code(332, 'ERROR_UNSUPPORTED_DATAFILE_FORMAT',
-            message='The data file format is not supported by the parser')
-        spec.exit_code(333, 'ERROR_OUTPUT_DATAFILE_PARSE',
-            message='The formatted data output file `{filename}` could not be parsed: {exception}')
+        spec.exit_code(
+            330,
+            'ERROR_OUTPUT_DATAFILE_MISSING',
+            message='The formatted data output file `{filename}` was not present in the retrieved (temporary) folder.'
+        )
+        spec.exit_code(
+            331, 'ERROR_OUTPUT_DATAFILE_READ', message='The formatted data output file `{filename}` could not be read.'
+        )
+        spec.exit_code(
+            332, 'ERROR_UNSUPPORTED_DATAFILE_FORMAT', message='The data file format is not supported by the parser'
+        )
+        spec.exit_code(
+            333,
+            'ERROR_OUTPUT_DATAFILE_PARSE',
+            message='The formatted data output file `{filename}` could not be parsed: {exception}'
+        )
 
-    def prepare_for_submission(
-        self, folder
-    ):  # pylint: disable=too-many-branches,too-many-statements
+    def prepare_for_submission(self, folder):  # pylint: disable=too-many-branches,too-many-statements
         """Prepare the calculation job for submission by transforming input nodes into input files.
 
         In addition to the input files being written to the sandbox folder, a `CalcInfo` instance will be returned that
@@ -121,8 +138,16 @@ class AverageCalculation(CalcJob):
         :return: :class:`~aiida.common.datastructures.CalcInfo` instance.
         """
 
-        remote_copy_list = [self._DEFAULT_INPUT_DATA_FILE]
+        remote_copy_list = []
         local_copy_list = []
+
+        source = self.inputs.get('parent_folder', None)
+
+        if isinstance(source, orm.RemoteData):
+            dirpath = os.path.join(source.get_remote_path(), self._DEFAULT_INPUT_DATA_FILE)
+            remote_copy_list.append((source.computer.uuid, dirpath, self._DEFAULT_INPUT_DATA_FILE))
+        elif isinstance(source, orm.FolderData):
+            local_copy_list.append((source.uuid, self._DEFAULT_INPUT_DATA_FILE, self._DEFAULT_INPUT_DATA_FILE))
 
         # Code information
         codeinfo = datastructures.CodeInfo()
@@ -143,11 +168,11 @@ class AverageCalculation(CalcJob):
         # Create the input file
         input_filename = self.inputs.metadata.options.input_filename
         with folder.open(input_filename, 'w') as infile:
-            infile.write("1\n")
-            infile.write(self._DEFAULT_INPUT_DATA_FILE + "\n")
-            infile.write("1.D0\n")
-            infile.write(f"{self.inputs.npts.value}\n")
-            infile.write(f"{self.inputs.average_axis.value}\n")
-            infile.write(f"{self.inputs.window_size.value / Bohr:.8f}\n")
+            infile.write('1\n')
+            infile.write(self._DEFAULT_INPUT_DATA_FILE + '\n')
+            infile.write('1.D0\n')
+            infile.write(f'{self.inputs.npts.value}\n')
+            infile.write(f'{self.inputs.average_axis.value}\n')
+            infile.write(f'{self.inputs.window_size.value / Bohr:.8f}\n')
 
         return calcinfo
